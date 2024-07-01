8c2ec738bf246b4abc9ec4a21380aa89525ea410
Kenward Terhemba <codewithkenward@gmail.com>
2024-07-01 18:33:37 +0100

    simple web api that collects user location for weather updates deploy update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const express = require('express');
 const axios = require('axios');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = async (req, res) => {
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name;
 
     try {
@@ -21,17 +27,20 @@ module.exports = async (req, res) => {
         const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.OPENWEATHER_API_KEY}`);
         const temperature = weatherResponse.data.main.temp;
 
-        // Dynamic greeting response
+        // Dynamic greeting respons
         const greeting = `Hello, ${visitorName}! The temperature is ${temperature} degrees Celsius in ${city}.`;
 
         res.json({
             client_ip: userIp,
             location: city,
-            greeting
-        });
+            greeting});
 
     } catch (error) {
         console.error('Error fetching data', error);
         res.status(500).json({ error: 'An error occurred while fetching data.' });
     }
-};
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
