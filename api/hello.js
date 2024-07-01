09ef716dc324244b7baf719a9b6b395f95eb8b64
Kenward Terhemba <codewithkenward@gmail.com>
2024-07-01 15:30:46 +0100

    test update

--- api/hello.js
+++ api/hello.js
@@ -1,16 +1,15 @@
-const express = require('express');
 const axios = require('axios');
-const app = express();
 require('dotenv').config();
-const port = process.env.PORT || 3000;
+
 const geoIpifyApiKey = process.env.geoIpifyApiKey;
 const openWeatherApiKey = process.env.openWeatherApiKey;
 
-app.get('/api/hello', async (req, res) => {
+module.exports = async (req, res) => {
     const visitorName = req.query.visitor_name || 'Visitor';
     const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
     try {
+        console.log(`Fetching geolocation data for IP: ${clientIp}`);
         // Get geolocation data
         const geoResponse = await axios.get(`https://geo.ipify.org/api/v2/country`, {
             params: {
@@ -18,8 +17,11 @@ app.get('/api/hello', async (req, res) => {
                 ipAddress: clientIp
             }
         });
+
+        console.log(`GeoResponse: ${JSON.stringify(geoResponse.data)}`);
         const { city } = geoResponse.data.location;
 
+        console.log(`Fetching weather data for city: ${city}`);
         // Get weather data
         const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
             params: {
@@ -28,6 +30,8 @@ app.get('/api/hello', async (req, res) => {
                 units: 'metric'
             }
         });
+
+        console.log(`WeatherResponse: ${JSON.stringify(weatherResponse.data)}`);
         const temperature = weatherResponse.data.main.temp;
 
         res.json({
@@ -36,10 +40,10 @@ app.get('/api/hello', async (req, res) => {
             greeting: `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${city}`
         });
     } catch (error) {
+        console.error(`Error fetching data: ${error.message}`);
+        if (error.response) {
+            console.error(`Error response data: ${JSON.stringify(error.response.data)}`);
+        }
         res.status(500).json({ error: 'Error fetching data' });
     }
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+};
